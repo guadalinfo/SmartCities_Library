6bc8ae1a0aee0e4391666ba798ef7e6a95ae0a57
javacasm <javacasm@gmail.com>
2017-07-23 01:07:38 +0200

    ejemplo

---
+++ Ejemplos/Base/_0_-_Plantilla/_0_-_Plantilla.ino
@@ -0,0 +1,39 @@
+/*
+Descripcion
+
+Conexiones
+
+
+ */
+
+
+/* ==== Includes - Librerias==== */
+
+/* ====  END Includes ==== */
+
+/* ==== Defines - Constantes ==== */
+/* ==== END Defines ==== */
+
+/* ==== Variables Globales ==== */
+
+/* ==== END Global Variables ==== */
+
+
+/* ==== Funciones a usar - Prototipos ==== */
+
+/* ==== END Prototypes ==== */
+
+/* ==== Setup - Configuracion==== */
+void setup() {
+  
+}
+/* ==== END Setup ==== */
+
+/* ==== Loop - Codigo que se repite==== */
+void loop() {
+}
+/* ==== End Loop ==== */
+
+/* ==== Funciones ==== */
+
+/* ==== END Functions ==== */
