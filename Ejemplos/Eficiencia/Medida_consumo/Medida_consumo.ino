50a367fdddd09a24c2b656f7e03a1b7a0f87231d
javacasm <javacasm@gmail.com>
2017-07-23 14:03:32 +0200

    Consumo

--- Ejemplos/Eficiencia/Medida_consumo/Medida_consumo.ino
+++ Ejemplos/Eficiencia/Medida_consumo/Medida_consumo.ino
@@ -11,38 +11,50 @@ http://www.arduino.org/learning/tutorials/boards-tutorials/webserverblink
 
 #include <Wire.h>
 #include <UnoWiFiDevEd.h>
+
 #define IN1 11
 #define IN2 10
 #define IN3 9
 #define IN4 8
-#define PIN_SENSOR_CORRIENTE A0
+
+#define PIN_SENSOR_CORRIENTE_1 A0
+#define PIN_SENSOR_CORRIENTE_2 A3
+
+#define pre F("<input type=button onClick=\"var w=window.open('/arduino/dig/")
+#define post F("','_parent');w.close();\" value='")
+
 
 // Sensibilidad del sensor en V/A
 //float SENSIBILITY = 0.185;   // Modelo 5A
 //float SENSIBILITY = 0.100; // Modelo 20A
 float SENSIBILITY = 0.066; // Modelo 30A
- 
-int SAMPLESNUMBER = 20;
+
+int SAMPLESNUMBER = 100;
+
+int iPines[]={IN1,IN2,IN3,IN4};
 
 void setup() {
-   Serial.begin(9600);
-    pinMode(IN1,OUTPUT);
-    pinMode(IN2,OUTPUT);
-    pinMode(IN3,OUTPUT);
-    pinMode(IN4,OUTPUT);
-    Wifi.begin();
-    Wifi.println("Web Server is up");
+ Serial.begin(9600);
+ for(int i=0;i<4;i++)   {
+    pinMode(iPines[i],OUTPUT);
+    digitalWrite(iPines[i],HIGH);
+  }
+  Wifi.begin();
+  Wifi.println("Web Server is up");
 }
 
-float corriente;
+float corriente1,corriente2;
 
 void loop() {
 
     while(Wifi.available()){
       process(Wifi);
     }
-    corriente=getCorriente(SAMPLESNUMBER);
-    Serial.println(corriente);
+    corriente1=getCorriente(SAMPLESNUMBER,PIN_SENSOR_CORRIENTE_1);
+    Serial.print(corriente1);
+    Serial.print(", ");
+    corriente2=getCorriente(SAMPLESNUMBER,PIN_SENSOR_CORRIENTE_2);
+    Serial.println(corriente2);
   delay(50);
 }
 
@@ -62,40 +74,44 @@ void process(WifiData client) {
 
 void WebServer(WifiData client) {
 
-  client.println("HTTP/1.1 200 OK");
-  client.println("Content-Type: text/html");
+  client.println(F("HTTP/1.1 200 OK"));
+  client.println(F("Content-Type: text/html"));
   client.println();
-  client.println("<html>");
+  client.println(F("<html>" \
+                  "<head> </head>" \
+                  "<body>"));
 
-  client.println("<head> </head>");
-  client.print("<body>");
   String pre=F("<input type=button onClick=\"var w=window.open('/arduino/dig/");
-  String post=F(" ','_parent');w.close();\" value=");
-  for(int i=8;i<12;i++)
+  String post=F("','_parent');w.close();\" value='");
+  for(int i=0;i<4;i++)
   {
-    String s=String(i);
-    
-    client.print(s+pre+s+"/1"+post+"'ON'>");
-    client.print(pre+s+"/0"+post+"'Off'><br>");
-    
+    String s=String(iPines[i]);
+    String sTatus=digitalRead(iPines[i])?"/1":"/0";
+    String sNombre=digitalRead(iPines[i])?"ON":"OFF";
+    client.print(s+pre+s+sTatus+post+sNombre+"'><br>");
   }
-  client.print("<p>Corriente:");
-  client.print(corriente);
+
+  client.print(F("<p>Corrientes:"));
+  client.print(corriente1);
+  client.print(",");
+  client.print(corriente2);
   client.println("</p>");
-  client.print("</body>");
-  client.println("</html>");
+
+  client.print(F("</body>" \
+                  "</html>"));
+
   client.print(DELIMITER); // very important to end the communication !!!
-  
+
 }
 
 
-float getCorriente(int samplesNumber)
+float getCorriente(int samplesNumber,int iPin)
 {
    float voltage;
    float corrienteSum = 0;
    for (int i = 0; i < samplesNumber; i++)
    {
-      voltage = analogRead(A0) * 5.0 / 1023.0;
+      voltage = analogRead(iPin) * 5.0 / 1023.0;
       corrienteSum += (voltage - 2.5) / SENSIBILITY;
    }
    return(corrienteSum / samplesNumber);
@@ -111,8 +127,10 @@ void digitalCommand(WifiData client) {
   // with a value like: "/d/13/1"
   if (client.read() == '/') {
     value = client.parseInt();
-    digitalWrite(pin, !value);
+    //digitalWrite(pin, !value);
+    digitalWrite(pin, !digitalRead(pin));
   }
+
 
   // Send feedback to client
   client.println("Status: 200 OK\n");
@@ -122,4 +140,5 @@ void digitalCommand(WifiData client) {
   client.print(value);
   client.print(EOL);    //char terminator
 
+  WebServer(client);
 }
