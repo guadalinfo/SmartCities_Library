0c88b78a0922fa307093ad31ee8c0400dad6ff77
javacasm <javacasm@gmail.com>
2017-07-28 16:05:38 +0200

    lcd 2 ids

--- examples/Base/_3_LCD/_3_LCD.ino
+++ examples/Base/_3_LCD/_3_LCD.ino
@@ -19,11 +19,13 @@ SCK (Serial Clock)  ->  A5 on Uno/Pro-Mini, 21 on Mega2560/Due, 3 Leonardo/Pro-M
 /* ====  END Includes ==== */
 
 /* ==== Defines ==== */
+#define SERIAL_BAUD 9600
 /* ==== END Defines ==== */
 
 /* ==== Global Variables ==== */
-// Set the LCD address to 0x27 for a 16 chars and 2 line display
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+// Set the LCD address to 0x27 or 0x3F for a 16 chars and 2 line display
+//LiquidCrystal_I2C lcd(0x27, 16, 2);
+LiquidCrystal_I2C lcd(0x3F, 16, 2);
 
 /* ==== END Global Variables ==== */
 
@@ -33,7 +35,7 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 /* ==== Setup ==== */
 void setup() {
-  setup_Serial();  
+  setup_Serial();
   setup_LCD();
 }
 /* ==== END Setup ==== */
@@ -55,13 +57,13 @@ void loop() {
 void setup_Serial(){
   Serial.begin(SERIAL_BAUD);
   while(!Serial) {} // Wait
-  
+
 }
 
 
 void setup_LCD(){
   lcd.init();         // Inicializamos el LCD
-  lcd.backlight();    // Encendemos la luz 
+  lcd.backlight();    // Encendemos la luz
 }
 
 
