11f3daecc9373585a742152a71e4ab61c1e030c4
javacasm <javacasm@gmail.com>
2017-07-29 12:51:17 +0200

    update

--- examples/Domotica/Midiendo_Temperaturas/Midiendo_Temperaturas.ino
+++ examples/Domotica/Midiendo_Temperaturas/Midiendo_Temperaturas.ino
@@ -3,7 +3,7 @@
 
 Medida de temperatura y humedad con 2 sensores DHT22
 
-Conexiones
+Conexiones DHT22
 * Pin +    a Vcc
 * Pin -    a GND
 * Pin out  a S
@@ -19,13 +19,15 @@ Un sensor DHT22 al pin 4 y otro al 6
 #include <DHT_U.h>
 
 /* ====  END Includes ==== */
-#define DHT1_PIN            6         
-#define DHT2_PIN            4
 
 
 /* ==== Defines - Constantes ==== */
 
-//#define DHTTYPE           DHT11     // DHT 11 
+#define DHT1_PIN            6
+#define DHT2_PIN            4
+
+
+//#define DHTTYPE           DHT11     // DHT 11
 #define DHTTYPE           DHT22     // DHT 22 (AM2302)
 //#define DHTTYPE           DHT21     // DHT 21 (AM2301)
 
@@ -60,34 +62,34 @@ void setup_DHT(){
   dht1.temperature().getSensor(&sensor);
   Serial.println("Temperatura");
   showSensorData(sensor);
-  
+
   dht1.humidity().getSensor(&sensor);
   Serial.println("Humedad");
   showSensorData(sensor);
-  
-  Serial.println("------------------------------------");  
-  
+
+  Serial.println("------------------------------------");
+
   Serial.println("Sensor 2");
   dht2.temperature().getSensor(&sensor);
   Serial.println("Temperatura");
   showSensorData(sensor);
-  
+
   dht2.humidity().getSensor(&sensor);
   Serial.println("Humedad");
   showSensorData(sensor);
- 
+
   // Set delay between sensor readings based on sensor details.
   delayMS = sensor.min_delay / 1000;
 }
 
 void setup_Serial(){
-  Serial.begin(9600); 
+  Serial.begin(9600);
 }
 
 void setup() {
   setup_Serial();
   setup_DHT();
-  
+
 }
 /* ==== END Setup ==== */
 
@@ -106,7 +108,7 @@ void showSensorData(sensor_t sensor){
   Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
   Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" *C");
   Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" *C");
-  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" *C");  
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" *C");
   Serial.println("------------------------------------");
 }
 
@@ -123,7 +125,7 @@ void showDataTemperatura(sensors_event_t event){
 }
 
 void showDataHumedad(sensors_event_t event){
-  
+
   if (isnan(event.relative_humidity)) {
     Serial.println("Error reading humidity!");
   }
@@ -138,12 +140,12 @@ void showData() {
   // Delay between measurements.
 
   // Get temperature event and print its value.
-  sensors_event_t event;  
+  sensors_event_t event;
   dht1.temperature().getEvent(&event);
   showDataTemperatura(event);
   dht2.temperature().getEvent(&event);
   showDataTemperatura(event);
-  
+
   // Get humidity event and print its value.
   dht1.humidity().getEvent(&event);
   showDataHumedad(event);
