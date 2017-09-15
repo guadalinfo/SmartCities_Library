a1bbf92392506e7bda506b4abd8a6e77d62644c2
javacasm <javacasm@gmail.com>
2017-09-16 00:51:40 +0200

    v1 filtros para AC

--- examples/Eficiencia/Regleta/Regleta.ino
+++ examples/Eficiencia/Regleta/Regleta.ino
@@ -20,6 +20,9 @@ Sensor de corriente ->  pin A0
 #include <Wire.h>               // Needed for legacy versions of Arduino.
 #include <LiquidCrystal_I2C.h>  // Pantalla LCD
 
+#include <Filters.h>
+
+
 /* ====  END Includes ==== */
 
 /* ==== Defines ==== */
@@ -35,6 +38,20 @@ Sensor de corriente ->  pin A0
 // Set the LCD address to 0x27 or 0x3F for a 16 chars and 2 line display
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 //LiquidCrystal_I2C lcd(0x3F, 16, 2);
+
+// Medida de corriente
+
+float testFrequency = 60;                     // test signal frequency (Hz)
+float windowLength = 20.0/testFrequency;     // how long to average the signal, for statistist
+int sensorValue = 0;
+float intercept = -0.1129; // to be adjusted based on calibration testing
+float slope = 0.0405; // to be adjusted based on calibration testing
+float current_amps; // estimated actual current in amps
+
+unsigned long printPeriod = 1000; // in milliseconds
+// Track time in milliseconds since last reading
+unsigned long previousMillis = 0;
+
 
 /* ==== END Global Variables ==== */
 
@@ -71,12 +88,30 @@ void setup() {
 
 /* ==== Loop ==== */
 void loop() {
-   int iValorSensorCorriente=analogRead(PIN_SENSOR_CORRIENTE);
 
-   lcd.setCursor(0,1);
-   lcd.print(iValorSensorCorriente);
-   Serial.println(iValorSensorCorriente);
-   delay(20);
+  RunningStatistics inputStats;                 // create statistics to look at the raw test signal
+  inputStats.setWindowSecs( windowLength );
+
+  while( true ) {
+    sensorValue = analogRead(A0);  // read the analog in value:
+    inputStats.input(sensorValue);  // log to Stats function
+
+    if((unsigned long)(millis() - previousMillis) >= printPeriod) {
+      previousMillis = millis();   // update time
+
+      // display current values to the screen
+      Serial.print( "\n" );
+      // output sigma or variation values associated with the inputValue itsel
+      Serial.print( "\tsigma: " ); Serial.print( inputStats.sigma() );
+      // convert signal sigma value to current in amps
+      current_amps = intercept + slope * inputStats.sigma();
+      Serial.print( "\tamps: " ); Serial.print( current_amps );
+      lcd.setCursor(0,0);
+      lcd.print(current_amps);
+    }
+  }
+
+
 }
 /* ==== End Loop ==== */
 
