4f8e645562c6cc5afdb742341c6abde9d18d9343
javacasm <javacasm@gmail.com>
2017-09-16 14:30:49 +0200

    add rtc

--- examples/Eficiencia/Regleta/Regleta.ino
+++ examples/Eficiencia/Regleta/Regleta.ino
@@ -1,5 +1,6 @@
 /*
 Control remoto de una regleta que mide el consumo y lo muestra por el LCD
+Usa RTC para establecer con precision el tiempo
 
 Conexion del LCD
 -----------------------------
@@ -11,6 +12,14 @@ SCK (Serial Clock)  ->  A5 on Uno/Pro-Mini, 21 on Mega2560/Due, 3 Leonardo/Pro-M
 Relé                ->  pin 11
 
 Sensor de corriente ->  pin A0
+
+Conexion del RTC 1302
+
+VCC      5V
+Gnd     GND
+SCK_PIN  4
+IO_PIN  5  //También conocido como Data
+CE_PIN  6  //También conocido como RST
 
  */
 
@@ -24,6 +33,9 @@ Sensor de corriente ->  pin A0
 // Libreria para filtrado
 #include <Filters.h>
 
+// Librerias para RTC
+#include <DS1302RTC.h>
+#include <Time.h>
 
 /* ====  END Includes ==== */
 
@@ -33,6 +45,10 @@ Sensor de corriente ->  pin A0
 #define PIN_SENSOR_CORRIENTE A0
 
 #define PIN_RELE 11
+
+#define SCK_PIN 4
+#define IO_PIN  5
+#define CE_PIN  6
 
 /* ==== END Defines ==== */
 
@@ -54,6 +70,9 @@ unsigned long printPeriod = 1000; // in milliseconds
 // Track time in milliseconds since last reading
 unsigned long previousMillis = 0;
 
+// RTC DS1302
+// Set pins:  CE, IO,CLK
+DS1302RTC RTC(CE_PIN, IO_PIN, SCK_PIN);
 
 /* ==== END Global Variables ==== */
 
@@ -79,17 +98,60 @@ void setup_Rele(){
   pinMode(PIN_RELE,OUTPUT);
 }
 
+void setup_RTC(){
+  lcd.print("RTC activado");
+
+  delay(500);
+
+  lcd.clear();
+  if (RTC.haltRTC()) {
+    lcd.print(F("RTC parado!"));
+  }
+  else
+    lcd.print(F("RTC Funcionando"));
+
+  lcd.setCursor(0,1);
+  if (RTC.writeEN())
+    lcd.print(F("Escritura Permitida"));
+  else
+    lcd.print(F("Escritura Bloqueada"));
+
+  delay ( 2000 );
+
+  // Setup time library
+  lcd.clear();
+  lcd.print(F("RTC Sincronizando"));
+  setSyncProvider(RTC.get);          // the function to get the time from the RTC
+  if(timeStatus() == timeSet)
+    lcd.print(F(" Ok!"));
+  else
+    lcd.print(F(" Error!"));
+
+  delay ( 2000 );
+
+  lcd.clear();
+
+}
+
+
 void setup() {
   setup_Serial();
   setup_LCD();
   setup_Rele();
 
   enciende_Rele();
+
+  setup_RTC();
 }
 /* ==== END Setup ==== */
 
 /* ==== Loop ==== */
 void loop() {
+
+  // Para ajustar el tiempo si es necesario
+  if (Serial.available() >= 12){
+    setTime();
+  }
 
   RunningStatistics inputStats;                 // create statistics to look at the raw test signal
   inputStats.setWindowSecs( windowLength );
@@ -111,6 +173,9 @@ void loop() {
       lcd.setCursor(0,0);
       lcd.print(current_amps);
     }
+  lcd.setCursor(0,1);
+  showTime();
+  Serial.println();
   }
 
 
@@ -127,4 +192,60 @@ void apaga_Rele(){
 }
 
 
+void setTime(){
+
+    //check for input to set the RTC, minimum length is 12, i.e. yy,m,d,h,m,s
+     {
+        tmElements_t tm;
+        time_t t;
+        //note that the tmElements_t Year member is an offset from 1970,
+        //but the RTC wants the last two digits of the calendar year.
+        //use the convenience macros from Time.h to do the conversions.
+        int y = Serial.parseInt();
+        if (y >= 100 && y < 1000)
+            lcd.println(F("Error: Year must be two digits or four digits!"));
+        else {
+            if (y >= 1000)
+                tm.Year = CalendarYrToTm(y);
+            else    //(y < 100)
+                tm.Year = y2kYearToTm(y);
+            tm.Month = Serial.parseInt();
+            tm.Day = Serial.parseInt();
+            tm.Hour = Serial.parseInt();
+            tm.Minute = Serial.parseInt();
+            tm.Second = Serial.parseInt();
+            t = makeTime(tm);
+      //use the time_t value to ensure correct weekday is set
+            if(RTC.set(t) == 0) { // Success
+              setTime(t);
+              lcd.clear();
+              lcd.print(F("RTC set!"));
+      }       else  {
+              lcd.clear();
+              lcd.print(F("RTC not set!"));
+      }
+            //dump any extraneous input
+            while (Serial.available() > 0) Serial.read();
+        }
+    }
+
+}
+void printSerialAndLCD(String str){
+  Serial.print(str);
+  lcd.print(str);
+}
+void print2Digit(int  valor){
+  if(valor<10)
+    printSerialAndLCD("0");
+  printSerialAndLCD(String(valor));
+}
+
+void showTime(){
+  print2Digit(hour());
+  printSerialAndLCD(":");
+  print2Digit(minute());
+  printSerialAndLCD(":");
+  print2Digit(second());
+
+}
 /* ==== END Functions ==== */
