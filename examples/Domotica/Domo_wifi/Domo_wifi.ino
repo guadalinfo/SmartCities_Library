20a49162e9e8649b5aef97ad5b776853ced02221
javacasm <javacasm@gmail.com>
2017-10-05 14:19:55 +0200

    domotica controlable por wifi

--- examples/Domotica/Domo_wifi/Domo_wifi.ino
+++ examples/Domotica/Domo_wifi/Domo_wifi.ino
@@ -18,8 +18,16 @@ activamos la iluminacion
 Conexiones
 Potenciometro  A0
 LDR           A2
-
 Luz           13
+
+Control remoto de 4 reles
+http://<IP>/arduino/webserver/
+
+Pines 9-12
+
+Alarma de presencia PIR
+Alarma  Pin 2
+Luz alarma 7
 
  */
 
@@ -31,17 +39,23 @@ Luz           13
 
 #include <Servo.h>
 
+#include <Wire.h>
+#include <UnoWiFiDevEd.h>
+
 /* ====  END Includes ==== */
 #define DHT1_PIN            6
 #define DHT2_PIN            4
 #define SERVO_PIN           8
 
 #define ALARMA_PIN          2
+#define LED_ALARMA_PIN      7
 
-#define IN1 11
-#define IN2 10
-#define IN3 9
-#define IN4 8
+// Pines de control de los Reles
+
+#define IN1 12
+#define IN2 11
+#define IN3 10
+#define IN4 9
 
 /* ==== Defines - Constantes ==== */
 
@@ -72,6 +86,11 @@ float temperatura_interior;
 float temperatura_exterior;
 
 Servo servo;
+
+int reles[]={IN1,IN2,IN3,IN4};
+boolean bAlarma=false;
+boolean bLuz=false;
+
 /* ==== END Global Variables ==== */
 
 
@@ -88,32 +107,48 @@ void setup_iluminacion(){
   pinMode(PIN_ILUMNINACION,OUTPUT);
 }
 
+void setup_alarma(){
+  pinMode(ALARMA_PIN,INPUT);
+  pinMode(LED_ALARMA_PIN,OUTPUT);
+}
+void setup_reles(){
+  for(int i=0;i<sizeof(reles);i++){
+    pinMode(reles[i],OUTPUT);
+    digitalWrite(reles[i],HIGH);
+  }
+}
+
+void setup_wifi(){
+    Wifi.begin();
+    Wifi.println(F("Web Server On"));
+}
+
 void setup_DHT(){
   dht1.begin();
   dht2.begin();
 
-  Serial.println("DHTxx Unified Sensor Example");
+  Serial.println(F("DHTxx Unified Sensor Example"));
   // Print temperature sensor details.
   sensor_t sensor;
 
-  Serial.println("Sensor 1");
+  Serial.println(F("Sensor 1"));
   dht1.temperature().getSensor(&sensor);
-  Serial.println("Temperatura");
+  Serial.println(F("Temperatura"));
   showSensorData(sensor);
 
   dht1.humidity().getSensor(&sensor);
-  Serial.println("Humedad");
+  Serial.println(F("Humedad"));
   showSensorData(sensor);
 
-  Serial.println("------------------------------------");
+  Serial.println(F("------------------------------------"));
 
-  Serial.println("Sensor 2");
+  Serial.println(F("Sensor 2"));
   dht2.temperature().getSensor(&sensor);
-  Serial.println("Temperatura");
+  Serial.println(F("Temperatura"));
   showSensorData(sensor);
 
   dht2.humidity().getSensor(&sensor);
-  Serial.println("Humedad");
+  Serial.println(F("Humedad"));
   showSensorData(sensor);
 
   // Set delay between sensor readings based on sensor details.
@@ -129,7 +164,9 @@ void setup() {
   setup_Servo();
   setup_DHT();
   setup_iluminacion();
-
+  setup_alarma();
+  setup_reles();
+  setup_wifi();
 }
 /* ==== END Setup ==== */
 
@@ -163,19 +200,27 @@ void loop() {
   } else {
     apagaLuz();
   }
-  
 
+  if(digitalRead(ALARMA_PIN)==HIGH){
+    enciendeAlarma();
+  } else {
+    apagaAlarma();
+  }
+
+  while(Wifi.available()){
+     process(Wifi);
+   }
 }
 /* ==== End Loop ==== */
 
 /* ==== Funciones ==== */
 void showSensorData(sensor_t sensor){
-  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
-  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
-  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
-  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" *C");
-  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" *C");
-  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" *C");
+  Serial.print  (F("Sensor:       ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:   ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:    ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:    ")); Serial.print(sensor.max_value); Serial.println(F(" *C"));
+  Serial.print  (F("Min Value:    ")); Serial.print(sensor.min_value); Serial.println(F(" *C"));
+  Serial.print  (F("Resolution:   ")); Serial.print(sensor.resolution); Serial.println(F(" *C"));
   Serial.println("------------------------------------");
 }
 
@@ -194,21 +239,114 @@ void getData() {
 
 void cierraVentanas(){
   servo.write(20);
-  Serial.println("Cerrando Ventanas");
+  Serial.println(F("Cerrando Ventanas"));
 }
 
 void abreVentanas(){
   servo.write(160);
-  Serial.println("Abriendo Ventanas");
+  Serial.println(F("Abriendo Ventanas"));
 }
+
+void enciendeAlarma(){
+  digitalWrite(LED_ALARMA_PIN,HIGH);
+  bAlarma=true;
+  Serial.println(F("Alarma encendida"));
+}
+
+void apagaAlarma(){
+  digitalWrite(LED_ALARMA_PIN,LOW);
+  bAlarma=false;
+  Serial.println(F("Alarma apagada"));
+}
+
 
 void enciendeLuz(){
   digitalWrite(PIN_ILUMNINACION,HIGH);
-  Serial.println("iluminacion encendida");
+  bLuz=true;
+  Serial.println(F("Iluminacion encendida"));
 }
 
 void apagaLuz(){
   digitalWrite(PIN_ILUMNINACION,LOW);
-  Serial.println("iluminacion apagada");
+  bLuz=false;
+  Serial.println(F("Iluminacion apagada"));
 }
+
+void process(WifiData client) {
+  // read the command
+  String command = client.readStringUntil('/');
+
+  // is "digital" command?
+  if (command == F("webserver")) {
+    WebServer(client);
+  }
+
+  if (command == F("dig")) {
+    digitalCommand(client);
+  }
+}
+
+void WebServer(WifiData client) {
+
+  client.println(F("HTTP/1.1 200 OK"));
+  client.println(F("Content-Type: text/html"));
+  client.println();
+  client.println(F("<html>"));
+
+  client.println(F("<head> </head>"));
+  client.print(F("<body>"));
+  String pre=F("<input type=button onClick=\"var w=window.open('/arduino/dig/");
+
+  for(int i=0;i<sizeof(reles);i++)
+  {
+    String s=String(reles[i]);
+
+    client.print(s+pre+s+F("/1','_parent');w.close();\"value='ON'>"));
+    client.print(pre+s+F("/0','_parent');w.close();\"value='Off'><br>"));
+  }
+  client.print(F("<br/> Temp ext.: "));
+  client.print(temperatura_exterior);
+  client.print(F("<br/> Temp int.: "));
+  client.print(temperatura_interior);
+  if(bAlarma==true){
+    client.print(F("<br/> Alarma ON"));
+  } else {
+    client.print(F("<br/> Alarma OFF"));
+  }
+
+  if(bLuz==true){
+    client.print(F("<br/> Luz ON"));
+  } else {
+    client.print(F("<br/> Luz OFF"));
+  }
+
+  client.print(F("</body>"));
+  client.println(F("</html>"));
+  client.print(DELIMITER); // very important to end the communication !!!
+
+}
+
+void digitalCommand(WifiData client) {
+  int pin, value = 0;
+
+  // Read pin number
+  pin = client.parseInt();
+
+  // If the next character is a '/' it means we have an URL
+  // with a value like: "/d/13/1"
+  if (client.read() == '/') {
+    value = client.parseInt();
+    digitalWrite(pin, !value);
+  }
+
+  // Send feedback to client
+  client.println(F("Status: 200 OK\n"));
+  client.print(F("Pin D"));
+  client.print(pin);
+  client.print(F(" set to "));
+  client.print(value);
+  client.print(EOL);    //char terminator
+
+}
+
 /* ==== END Functions ==== */
