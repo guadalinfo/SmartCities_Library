3053bde6c1e22a0ded9b0b4dfade1162de07e84f
javacasm <javacasm@gmail.com>
2017-10-21 19:39:36 +0200

    url

--- examples/Domotica/_3_Sensor_Atmosferico_BME280/_3_Sensor_Atmosferico_BME280.ino
+++ examples/Domotica/_3_Sensor_Atmosferico_BME280/_3_Sensor_Atmosferico_BME280.ino
@@ -1,33 +1,14 @@
 /*
-BME280 I2C Test.ino
-This code shows how to record data from the BME280 environmental sensor
-using I2C interface. This file is an example file, part of the Arduino
-BME280 library.
-Copyright (C) 2016  Tyler Glenn
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+Lectura de datos atmosferico que se muestran en el LCD
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+https://github.com/javacasm/SmartCities_Domotica#sensor-bme280
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-Written: Dec 30 2015.
-Last Updated: Sep 19 2016.
-
-Connecting the BME280 Sensor:
-Sensor              ->  Board
 -----------------------------
-Vin (Voltage In)    ->  3.3V
+Vin (Voltage In)    ->  Vcc
 Gnd (Ground)        ->  Gnd
-SDA (Serial Data)   ->  A4 on Uno/Pro-Mini, 20 on Mega2560/Due, 2 Leonardo/Pro-Micro
-SCK (Serial Clock)  ->  A5 on Uno/Pro-Mini, 21 on Mega2560/Due, 3 Leonardo/Pro-Micro
+SDA (Serial Data)   ->  A4
+SCK (Serial Clock)  ->  A5
 
  */
 
