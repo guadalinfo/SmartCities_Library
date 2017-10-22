950f4de688b638217fcb0eaab812c34ddbd78ec3
javacasm <javacasm@gmail.com>
2017-10-22 23:21:30 +0200

    API

--- examples/Base/otros/Publicacion_suscripcion_datos/Publicacion_suscripcion_datos.ino
+++ examples/Base/otros/Publicacion_suscripcion_datos/Publicacion_suscripcion_datos.ino
@@ -82,8 +82,8 @@ void setup() {
     Serial.println("Could not find BME280 sensor!");
     delay(1000);
   }
-  
-  ////lcd.init();   
+
+  ////lcd.init();
   ////lcd.backlight();
 
 
@@ -97,14 +97,14 @@ void setup() {
 /* ==== Loop ==== */
 void loop() {
    if((millis()-ultima_medida)>intervalo)    {
-    
+
     printBME280Data(&Serial);
     printBME280CalculatedData(&Serial);
-    
+
     Ciao.write(CONNECTOR, TOPIC_TEMP, String(temp)); // pushes data into a channel called PIN_A1
     Ciao.write(CONNECTOR, TOPIC_PRES, String(pres)); // pushes data into a channel called PIN_A1
     Ciao.write(CONNECTOR, TOPIC_HUM, String(hum)); // pushes data into a channel called PIN_A1
-    
+
     ultima_medida=millis();
    }
 //    while(Wifi.available()){
@@ -125,9 +125,9 @@ CiaoData data = Ciao.read(CONNECTOR, TOPIC_LIGHT);
   digitalWrite(LED,LOW);
   Serial.println(" off!!");
  }
- 
+
  }
-    
+
    delay(50);
 }
 /* ==== End Loop ==== */
@@ -136,7 +136,7 @@ CiaoData data = Ciao.read(CONNECTOR, TOPIC_LIGHT);
 /* ==== Functions ==== */
 void printBME280Data(Stream* client){
 
-   uint8_t pressureUnit(3);                                           // unit: B000 = Pa, B001 = hPa, B010 = Hg, B011 = atm, B100 = bar, B101 = torr, B110 = N/m^2, B111 = psi
+                                        // unit: B000 = Pa, B001 = hPa, B010 = Hg, B011 = atm, B100 = bar, B101 = torr, B110 = N/m^2, B111 = psi
    bme.read(pres, temp, hum, metric, pressureUnit);                   // Parameters: (float& pressure, float& temp, float& humidity, bool celsius = false, uint8_t pressureUnit = 0x0)
   /* Alternatives to ReadData():
     float temp(bool celsius = false);
@@ -217,7 +217,7 @@ void WebServer(WifiData client) {
   client.print(pres);
   client.print("<br/>");
 
-  
+
   client.print("</body>");
   client.println("</html>");
   client.print(DELIMITER); // very important to end the communication !!!
