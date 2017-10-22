e341b064ca616fb22d745732795350e5590a4f1c
javacasm <javacasm@gmail.com>
2017-10-22 23:21:30 +0200

    API

--- examples/Meteo/Publicacion_datos/Publicacion_datos.ino
+++ examples/Meteo/Publicacion_datos/Publicacion_datos.ino
@@ -77,8 +77,8 @@ void setup() {
     Serial.println("Could not find BME280 sensor!");
     delay(1000);
   }
-  
-  lcd.init();   
+
+  lcd.init();
   lcd.backlight();
 
 
@@ -92,18 +92,18 @@ void setup() {
 /* ==== Loop ==== */
 void loop() {
    if((millis()-ultima_medida)>intervalo)    {
-    
+
     printBME280Data(&Serial);
     printBME280CalculatedData(&Serial);
-    
+
     Ciao.write(CONNECTOR, TOPIC, String(22.6)); // pushes data into a channel called PIN_A1
-    
+
     ultima_medida=millis();
    }
     while(Wifi.available()){
       process(Wifi);
     }
-   
+
    delay(50);
 }
 /* ==== End Loop ==== */
@@ -112,7 +112,7 @@ void loop() {
 /* ==== Functions ==== */
 void printBME280Data(Stream* client){
 
-   uint8_t pressureUnit(3);                                           // unit: B000 = Pa, B001 = hPa, B010 = Hg, B011 = atm, B100 = bar, B101 = torr, B110 = N/m^2, B111 = psi
+                                          // unit: B000 = Pa, B001 = hPa, B010 = Hg, B011 = atm, B100 = bar, B101 = torr, B110 = N/m^2, B111 = psi
    bme.read(pres, temp, hum, metric, pressureUnit);                   // Parameters: (float& pressure, float& temp, float& humidity, bool celsius = false, uint8_t pressureUnit = 0x0)
   /* Alternatives to ReadData():
     float temp(bool celsius = false);
@@ -193,7 +193,7 @@ void WebServer(WifiData client) {
   client.print(pres);
   client.print("<br/>");
 
-  
+
   client.print("</body>");
   client.println("</html>");
   client.print(DELIMITER); // very important to end the communication !!!
